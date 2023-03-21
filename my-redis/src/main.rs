2cedc133ec1e792f952ef203ad1ae44cb125c171
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-03-22 08:47:28 +0900

    handle multiple commands

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -1,4 +1,6 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+
+use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -15,12 +17,25 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    let mut db = HashMap::<String, Vec<u8>>::new();
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Command::Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            Command::Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            _ => unimplemented!(),
+        };
 
-        let response = Frame::Error("unimplemented".to_string());
         connection.write_frame(&response).await.unwrap();
     }
 }
